58ec8fb638fe64cd8545beb7961dcfc974b98e57
Noz155 <64686364+Noz155@users.noreply.github.com>
2023-09-07 15:03:28 +0500

    Updated the main.py file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from discordbot.minigamesbot import MiniGamesBot
 from discordbot.utils.private import DISCORD
 
-bot = MiniGamesBot("?")
-bot.run(DISCORD["TOKEN"])
+bot = MiniGamesBot(".")
+bot.run(DISCORD["MTE0ODk1OTE1NTAzMDM0NzgyOA.GU34NU.0bzCw56vmC4MM2SUIEzZmTidYWqRBobiTMMcrI"])
